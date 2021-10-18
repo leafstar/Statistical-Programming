d89257aafb26fe771212a377153b0e5e354cf714
nutsa <n.tokhadze@iset.ge>
2021-10-18 19:33:38 +0100

    New changes

--- wg03p2.R
+++ wg03p2.R
@@ -5,19 +5,23 @@
 ##########################################
 ## Address of github repo:.....
                                                           
-                                                          ## Covid and the hazards of opportunistic sampling
+                                                       ## Covid and the hazards of opportunistic sampling
 
-## Our main task is to show what the daily infection trajectories look like for the population with the lowest individual transmission probabilities, and how
-## they compare to the infection trajectories for the whole population. 
-## For that purpose we split the population by 4 categories 1) Susceptible 2) Exposed 3) Infective and 4) Recovered. 
-## (1) Start the epidemic by setting 10 randomly chosen people to the Exposed state. Simulated for 100 model days, recorded the number of new infections each day. 
-## (2) Record the number of new infections among the 10% of the population with the lowest transmission rates. 
-## (3) Record the number of new infections in a random sample of 0.1% of the population.
-## (4) Produce a plot showing how the daily infection trajectories compare between the whole population, the ‘cautious 10%’ and the 0.1% random sample.
-## (5) Finally write code to visualize the variability in the results from simulation to simulation, by running 10 replicate simulations, and plotting these.
+## The Covid pandemic is an ongoing global pandemic already two years. One of the main features of the Covid-19 epidemic in the UK and generally in the world 
+## is that the estimates of the number of new cases per day are not always based on properly sampled statistical surveys. Thus, estimates are under question. 
+## The main purpose of this assignment is to show what the daily infection trajectories look like for the population with the lowest individual transmission 
+## probabilities, and how they compare to the infection trajectories for the whole population in the UK. 
+## For that purpose we split the population by 4 categories 1) Susceptible 2) Exposed 3) Infective and 4) Recovered and do following steps:
+  ## (1) Show daily infection trajectories for three groups 1) Whole population 2) Cautious group - 10% of the population with the lowest transmission
+  ## rates and 3) Random sample of 0.1% of the population.
+  ## (2) Produce a plot showing how the daily infection trajectories compare between the whole population, the ‘cautious 10%’ and the 0.1% random sample. 
+  ## (3) Run 10 replicate simulations for each groups.
+  ## (4) Produce three plots for 10 replicate simulations showing daily infection trajectories for each groups.
 
-   ## Step(1) Started the epidemic by setting 10 randomly chosen people to the Exposed state. Simulated for 100 days, recorded the number of new infections each day.
-## Defined Stochastic Simulation Model function 
+   ## Step(1):Show daily infection trajectories for three groups 1) Whole population 2) Cautious group - 10% of the population with the lowest transmission
+   ## rates and 3) Random sample of 0.1% of the population.
+## Defined Stochastic Simulation Model function
+## Started the epidemic by setting 10 randomly chosen people to the Exposed state and Simulated for 100 days.
 ## inputs are the parameters and the output is the number of people in each state each day.
 simulation<-function(n = 5.5e6, ne = 10, nt = 150, gamma = 1/3, delta = 1/5, lambda = 0.4/n){ 
   ## states: 0 -> S(Susceptible); 1 -> E(Exposed); 
@@ -25,15 +29,15 @@ simulation<-function(n = 5.5e6, ne = 10, nt = 150, gamma = 1/3, delta = 1/5, lam
   ## n = population size; ne =initial exposed; nt = number of days; 
   ## gamma=daily prob E->I, delta=daily prob I->R lambda=Overall viral infectivity parameter.
   
-  x <- rep(0,n) ## initialize to susceptible state
+  x <- rep(0,n)  ## initialize to susceptible state
   
   beta <- rlnorm(n,0,0.5); beta <- beta/mean(beta) ## generates contact rate with other people
   cautious_group = which(beta <= quantile(beta, 0.1)) ## 10% of the most cautious group
-  random_group = sample(1:n, n/1000) ## 
+  random_group = sample(1:n, n/1000) ## creat random sample of 0.1% of the population.
   initial_exposure = sample(1:n, ne) ## start with the sample 10 people in E state
-  x[initial_exposure] = 1
+  x[initial_exposure] = 1 ## creat some infectives
   I1 <- I2 <- I3 <- rep(0, nt) ## set up storage for number of infective people for 3 groups
-  for (i in 2:nt){
+  for (i in 2:nt){ ## loop over days
     u <- runif(n) ## uniform random deviates
     exposed_prob = lambda * beta * sum(beta[x==2]) ## Set probability of uninfected person of being infected and entering (E) state
     x[x==2 & u<delta] <- 3 ## I -> R with prob delta
@@ -41,17 +45,14 @@ simulation<-function(n = 5.5e6, ne = 10, nt = 150, gamma = 1/3, delta = 1/5, lam
     x[x==0 & u<exposed_prob] <- 1 ## S -> E with prob 'exposed_prob'
 
     ## record the number of new infections each day for 3 groups
-    I1[i] <- sum(x==2); 
-    I2[i] <- sum(x[cautious_group]==2);
-    I3[i] <- sum(x[random_group]==2);
+    I1[i] <- sum(x==2); ## record for total population
+    I2[i] <- sum(x[cautious_group]==2); ## record for cautiouse group
+    I3[i] <- sum(x[random_group]==2); ## record for random sample of 0.1% of the population
   }
   list(I1=I1,I2=I2,I3=I3,beta=beta)
 }
 
-## Step (2) Recorded the number of new infections among the 10% of the population with the lowest transmission rates
-
-   ## I think we should do this step somewhere in the end.
-
+    #Step (2):Produce a plot showing how the daily infection trajectories compare between the whole population, the ‘cautious 10%’ and the 0.1% random sample.
 a = Sys.time()
 epi <- simulation() ## run simulation
 b= Sys.time()
@@ -61,21 +62,22 @@ points(log(epi$I2),col=4,cex=0.1);points(log(epi$I3),col=2,cex=0.1)  ## E (blue)
 legend(1, 15, legend=c("whole population", "cautious group", "random 0.1% group"),
        col=c("black", "blue","red"), lty=1:2, cex=0.8)
 
-
+  ##Step (3):Run 10 replicate simulations for each groups 
 #The aim of this loop is tp generate the 10 simulations and then create a vector that stores the 10 outputs
 epi10=list()
 for (i in 1:10){
   epi10[[length(epi10)+1]] <- simulation()
 }
 
+  ##Step (4): Produce plots for 10 replicate simulations showing daily infection trajectories for the 1)whole population, the 2)‘cautious 10%’ and the 3)0.1% random sample.
 xlim = 150
-## plot for whole population
+##(4.1) plot for whole population
 plot(log(epi10[[1]]$I1),ylim=c(0,log(max(epi10[[1]]$I1))),xlab="day",ylab="log(new infection)",cex=0.1, main = "whole population",type="l")
 for (i in 2:10) {
   lines(1:xlim,log(epi10[[i]]$I1), col = rainbow(10)[i])
 }
 
-## Plot for the 10% of the population
+##(4.2) Plot for the cautious group-10% of the population
 plot(log(epi10[[1]]$I2),ylim=c(0,log(max(epi10[[1]]$I2))),xlab="day",ylab="log(new infection)",cex=0.1, main = "cautious group",type="l")
 for (i in 2:10) {
   
@@ -83,8 +85,7 @@ for (i in 2:10) {
   
 }
 
-
-## Plot for 0.1% of the population 
+## (4.3) Plot for random sample of 0.1% of the population 
 plot(log(epi10[[1]]$I3),ylim=c(0,log(max(epi10[[1]]$I3))),xlab="day",ylab="log(new infection)",cex=0.1, main = "0.1% group",type="l")
 for (i in 2:10) {
   lines(1:xlim,log(epi10[[i]]$I3), col = rainbow(10)[i])
